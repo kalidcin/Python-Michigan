d8b67489854345070f70b0d361cd4d947c653ee5
kalidcin <valteer.kalid@gmail.com>
2022-08-30 11:13:59 -0500

    commit CAMBIO 1

---
+++ Desempaquetado.py
@@ -0,0 +1,4 @@
+language = "python"
+a, b, c, d, e, f = language
+print(a)
+print(e)
---
+++ String.py
@@ -0,0 +1,2 @@
+name, surname, age = "Valter", "Quintanar", 27
+print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
